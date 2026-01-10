dbcac3aa12ff2086de6db792875d5c229c1cbf2e
moha4733 <moonyahidi@gmail.com>
2026-01-10 13:19:33 +0100

    inital spring boot project

---
+++ src/main/java/dk/tommer/workday/TommerApplication.java
@@ -0,0 +1,13 @@
+package dk.tommer.workday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TommerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TommerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/tommer/workday/TommerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.tommer.workday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TommerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
